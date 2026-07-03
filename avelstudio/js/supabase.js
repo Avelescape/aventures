8dd8ed0c985e9af8d4e3048d7ab501e93212e593
Avelescape <avelescape@gmail.com>
2026-07-03 13:53:05 +0200

    Update supabase.js

--- avelstudio/js/supabase.js
+++ avelstudio/js/supabase.js
@@ -1,1 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 
+<script>
+  const SUPABASE_URL = "https://braybbxyptumhtbfqoho.supabase.co";
+  const SUPABASE_KEY = "sb_publishable_2EdmGUgw5lo0NDgCDwmudA_kS8COBvo";
+
+  const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+</script>
